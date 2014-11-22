09c3e80f354ebfa59474d4e8247a43e70d302700
CrookedKings <John.Mahowald@gmail.com>
2014-11-22 14:42:46 -0800

    improved seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,30 @@ list1 = board1.lists.create!(title: 'Todo')
 list2 = board1.lists.create!(title: 'Doing')
 list3 = board1.lists.create!(title: 'Done')
 
-card1 = list1.cards.create!(title: 'Squats', description: 'feel the burn')
-card2 = list1.cards.create!(title: 'Pushups', description: 'ooh ouch')
-card3 = list1.cards.create!(title: 'Situps', description: 'ouchy')
+list1.cards.create!(title: 'Squats', description: 'feel the burn')
+list1.cards.create!(title: 'Pushups', description: 'ooh ouch')
+list1.cards.create!(title: 'Situps', description: 'ouchy')
 
-card4 = list2.cards.create!(title: 'Pullups', description: 'feel the burn')
-card5 = list2.cards.create!(title: 'Push Press', description: 'ooh ouch')
-card6 = list2.cards.create!(title: 'Clean & Jerk', description: 'ouchy')
+list2.cards.create!(title: 'Pullups', description: 'feel the burn')
+list2.cards.create!(title: 'Push Press', description: 'ooh ouch')
+list2.cards.create!(title: 'Clean & Jerk', description: 'ouchy')
 
-card7 = list3.cards.create!(title: 'Power Cleans', description: 'feel the burn')
-card8 = list3.cards.create!(title: 'L Hangs', description: 'ooh ouch')
-card9 = list3.cards.create!(title: 'Jumping Jacks', description: 'ouchy')
+list3.cards.create!(title: 'Power Cleans', description: 'feel the burn')
+list3.cards.create!(title: 'L Hangs', description: 'ooh ouch')
+list3.cards.create!(title: 'Jumping Jacks', description: 'ouchy')
+
+board2 = user1.boards.creaet!(title: "Jamaica")
+
+list4 = board2.lists.create!(title: "Visit")
+list4 = board2.lists.create!(title: "Eat")
+list4 = board2.lists.create!(title: "Listen")
+
+list4.cards.create!(title: 'Kingston', description: 'feel the burn')
+list4.cards.create!(title: 'Montego Bay', description: 'ooh ouch')
+list4.cards.create!(title: 'Annotto Bay', description: 'ouchy')
+
+list5.cards.create!(title: 'Guava', description: 'feel the burn')
+list5.cards.create!(title: 'Jackfruit', description: 'ooh ouch')
+list5.cards.create!(title: 'Pineapple', description: 'ouchy')
+
+list6.cards.create!(title: 'Reggae', description: 'feel the burn')
